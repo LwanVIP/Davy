62e28ee4c36bb30aa6c72f852ac1b3d018e55eea
Davy <15675750001@sina.cn>
2017-07-06 00:21:32 +0800

    Add files via upload

---
+++ 新老王-定制多合一/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using EloBuddy.SDK.Events;
+
+namespace UB_AIO
+{
+    class Program
+    {
+        private static readonly string getAppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        private static readonly string dllPath = Path.Combine(getAppDataPath, @"EloBuddy\Addons\Libraries\Lwan_VIPSeries1.dll");
+
+        private static void Main(string[] eventArgs)
+        {
+            Loading.OnLoadingComplete += Args =>
+            {
+                if (File.Exists(dllPath))
+                {
+                    File.Delete(dllPath);
+                }
+
+                var prdll = Properties.Resource.Lwan_VIPSeries1;
+                using (var fs = new FileStream(dllPath, FileMode.Create))
+                {
+                    fs.Write(prdll, 0, prdll.Length);
+                }
+                var dllfile = Assembly.LoadFrom(dllPath);
+                var type = dllfile.GetType("Lwan_VIPSeries1.dhy");
+                var main = type.GetMethod("Main");
+                main.Invoke(null, null /*new object[] { new string[] { } }*/);
+            };
+        }
+    }
+}
---
+++ 新老王-定制多合一/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UB-AIO")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UB-AIO")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("638b3066-0350-43d9-9cb0-44aeefbd37bf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("7.13.0.0")]
